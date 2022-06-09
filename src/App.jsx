38c513433ee47480add4a9a1c57a7cf46f1838ee
mireu-san <67046863+mireu-san@users.noreply.github.com>
2022-06-09 17:13:15 +0900

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,89 @@
 import React, { useState } from 'react'
-import './App.css'
-import { GrCheckbox as Box }  from 'react-icons/gr'
-import { GrCheckboxSelected as BoxTwo } from 'react-icons/gr' 
 
 export default function App() {
-    const [tick, setTick] = useState(false)
+    const [count, setCount] = useState(4)
+    const [theme, setTheme] = useState('sky')
 
-    const handleTick = () => {
-        setTick(!tick)
+    function decreaseCount() {
+        setCount(prevCount => prevCount - 1)
+        setTheme(prevCount => prevCount === 'sky' ? 'low' : 'sky')
+    }
+
+    function increaseCount() {
+        setCount(prevCount => prevCount + 1)
+        setTheme(prevCount => prevCount === 'sky' ? 'high' : 'sky')
     }
 
     return (
         <>
-            <div>
-                <p>This is a bookmark app. Tick box is not from web api (html). </p>
-                <p>You can toggle the box and see how it changes when you click.</p>
-            
-                <div className="TickIcon" 
-                    onClick={handleTick}>
-                    {tick ? (
-                        <Box className="tickFillIcon" />
-                        ) : (
-                        <BoxTwo className="tickIcon" />
-                    )}
-                </div>
-            </div>
+            <button onClick={decreaseCount}>-</button>
+            <span>{count}</span>
+            <span>{theme}</span>
+            <button onClick={increaseCount}>+</button>
         </>
     )
+    
 }
+
+
+
+
+
+
+
+
+// import React, { useState } from 'react'
+// import './App.css'
+// // import { GrCheckbox as Box }  from 'react-icons/gr'
+// // import { GrCheckboxSelected as BoxTwo } from 'react-icons/gr' 
+
+// export default function App() {
+//     const [count, setCount] = useState(0)
+
+//     function decrease() {
+//         setCount(prevCount => prevCount - 1)
+//     }
+//     function increase() {
+//         setCount(prevCount => prevCount + 1)
+//     }
+
+
+//     return (
+//         <>
+//             <button onClick={decrease}>-</button>
+//             <span>{count}</span>
+//             <button onClick={increase}>+</button>
+//         </>
+//     )
+// }
+// 잘 되는 버전 -  icon toggle feature (check, uncheck)
+// export default function App() {
+//     const [tick, setTick] = useState(false)
+
+//     const handleTick = () => {
+//         setTick(!tick)
+//     }
+
+//     return (
+//         <>
+//             <div>
+//                 <p>This is a bookmark app. Tick box is not from web api (html). </p>
+//                 <p>You can toggle the box and see how it changes when you click.</p>
+            
+//                 <div className="TickIcon" 
+//                     onClick={handleTick}>
+//                     {tick ? (
+//                         <Box className="tickFillIcon" />
+//                         ) : (
+//                         <BoxTwo className="tickIcon" />
+//                     )}
+//                 </div>
+//             </div>
+//         </>
+//     )
+// }
+
+
 
 
 // Almost worked
